9a5c3750df9ae3f2eacbcd907324874cfd278f2b
Icaro-Nunes <icarolimanunes@gmail.com>
2022-09-15 20:54:25 -0300

    Inserção de métodos em classe

--- binary_decision_tree.py
+++ binary_decision_tree.py
@@ -38,48 +38,6 @@ def calculate_distribution(x, y, value):
     set = x.join(y)
     return float(len(set[set[y.name] == value]))/len(set)
 
-
-def build_tree_node(x_subset, y_subset, unused_features: list):
-    subset = x_subset.join(y_subset)
-    distribution = calculate_distribution(x_subset, y_subset, 'Sim')
-
-    estimated_label = 'Sim' if distribution > 0.5 else 'Não'
-
-    if len(unused_features) == 0:
-        return DecisionTreeTerminalNode(
-            label=estimated_label
-        )
-
-    if distribution == 1.0 or distribution == 0.0:
-        return DecisionTreeTerminalNode(
-            label=estimated_label
-        )
-
-    best_feature = max(
-        map(
-        lambda feature: (feature, information_gain(subset, feature, y_subset.name))
-        , unused_features
-        )
-    )
-
-    feature = best_feature[0]
-
-    feature_values = x_subset[feature].unique()
-    
-    unused_features_passed = [feat for feat in unused_features if feat != feature]
-
-    children = {
-        value: build_tree_node(
-            x_subset[x_subset[feature] == value], subset[subset[feature] == value][y_subset.name], unused_features_passed
-        ) for value in feature_values
-    }
-
-    return DecisionTreeNode(
-        feature=feature,
-        children=children
-    )
-
-
 class DecisionTreeNode():
     def __init__(self, feature , children):
         self.feature = feature
@@ -102,9 +60,49 @@ class BinaryDecisionTree():
         self.root = None
         # self.current_node = None
 
+
+    def __build_tree_node(self, x_subset, y_subset, unused_features: list):
+        subset = x_subset.join(y_subset)
+        distribution = calculate_distribution(x_subset, y_subset, 'Sim')
+
+        estimated_label = 'Sim' if distribution > 0.5 else 'Não'
+
+        if len(unused_features) == 0:
+            return DecisionTreeTerminalNode(
+                label=estimated_label
+            )
+
+        if distribution == 1.0 or distribution == 0.0:
+            return DecisionTreeTerminalNode(
+                label=estimated_label
+            )
+
+        best_feature = max(
+            map(
+            lambda feature: (feature, information_gain(subset, feature, y_subset.name))
+            , unused_features
+            )
+        )
+
+        feature = best_feature[0]
+
+        feature_values = x_subset[feature].unique()
+        
+        unused_features_passed = [feat for feat in unused_features if feat != feature]
+
+        children = {
+            value: self.__build_tree_node(
+                x_subset[x_subset[feature] == value], subset[subset[feature] == value][y_subset.name], unused_features_passed
+            ) for value in feature_values
+        }
+
+        return DecisionTreeNode(
+            feature=feature,
+            children=children
+        )
     
     def fit(self, x: pd.DataFrame, y: pd.Series):
-        self.root = build_tree_node(x, y, list(x))
+        self.root = self.__build_tree_node(x, y, list(x))
         
     
     def predict(self, x):
